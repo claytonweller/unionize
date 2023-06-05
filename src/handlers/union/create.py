e6447589116ba676c65fa7aa57b988b71bf7f48d
calytonweller <clyaton.weller@gmail.com>
2023-06-05 15:11:06 -0600

    Saving generic union to db

--- src/handlers/union/create.py
+++ src/handlers/union/create.py
@@ -1,9 +1,23 @@
 from modules.lambda_response import format
+import boto3
+from json import loads
+
+
+dynamo = boto3.resource('dynamodb')
+union_table = dynamo.Table('unionize-unions')
 
 
 def handler(event, context):
     print(event)
     response = {
-        "event": event
+        "event": event,
     }
+    body = loads(event['body'])
+
+    item = {
+        'unionName': body['union-name'],
+        'someOtherKey': 'key',
+    }
+    saved_item = union_table.put_item(Item=item)
+    print(saved_item)
     return format(response)
