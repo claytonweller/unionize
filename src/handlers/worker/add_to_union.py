8fa1fcc553015fb5317773f063cfd1a4358d1f29
claytonweller <clyaton.weller@gmail.com>
2023-09-17 11:58:37 -0600

    Add sms logic for worker invitations
    We need to not spam people who have alread received the sms text.

--- src/handlers/worker/add_to_union.py
+++ src/handlers/worker/add_to_union.py
@@ -11,12 +11,12 @@ sns = client('sns')
 def handler(event, context):
     print(event)
     body = loads(event['Records'][0]['body'])
-
+    union_name = body['unionName']
     worker_table = WorkerTable()
-    union_workers = worker_table.get_workers_in_union(body['unionName'])
+    union_workers = worker_table.get_workers_in_union(union_name)
     worker = parse_worker_from_body(body)
     match = find_matching_union_worker(
-        union_workers, worker.phone)
+        union_workers, worker.phone, worker.email)
 
     if match:
         # TODO This could definitely be more robust
@@ -29,10 +29,12 @@ def handler(event, context):
 
     sns.publish(
         PhoneNumber=worker.phone,
-        Message="A coworker would like to start a union. Click here http----."
+        Message=f'You have been added to {union_name}. Finish signing up here - http----.'
     )
 
     return
+
+# TODO there is a bunch of duplication with the stuff in invite.py
 
 
 def parse_worker_from_body(body) -> Worker:
